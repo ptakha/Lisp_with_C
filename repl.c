a67fcd8ca538bdfe3772556edf18bf5500ad1448
ptakha <potoka1998@gmail.com>
2022-04-03 22:16:07 +0200

    Added fake readline functions if code will be compiled on Windows

--- repl.c
+++ repl.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
 
+static char input[2048];
+/*Not original readline function*/
+char* readline(char* prompt)
+{
+  fputs(prompt, stdout);
+  fgets(input, 2048, stdin);
+  char* copy = malloc(strlen(input)+1);
+  strcpy(copy, input);
+  copy[strlen(copy)-1]='\0';
+  return copy;
+}
+
+void add_history(char* something){}
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv)
 {
